713a5554f4077389a523b2a6a535e633858372b2
axelbernard20 <axel.bernard@etudiant.univ-rennes.fr>
2023-12-22 08:06:08 +0100

    Tuto jenkins

---
+++ test-plugin-jenkins/demo-plugin/src/main/java/io/jenkins/plugins/sample/HelloWorldAction.java
@@ -0,0 +1,33 @@
+package main.java.io.jenkins.plugins.sample;
+
+import java.io.IOException;
+
+import hudson.model.Action;
+
+public class HelloWorldAction implements Action{
+
+    private String name;
+
+    public HelloWorldAction(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "document.png";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Greeting";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "greeting";
+    }
+}

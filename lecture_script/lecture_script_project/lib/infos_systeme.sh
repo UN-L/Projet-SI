7581f2062b391cfa5fb49cb78a9003e69a953e4e
axelbernard20 <axel.bernard@etudiant.univ-rennes.fr>
2024-02-09 08:22:47 +0100

    lecture script

--- lecture_script/lecture_script_project/lib/infos_systeme.sh
+++ lecture_script/lecture_script_project/lib/infos_systeme.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Affichage du nom de l'hôte
+
 echo "Nom de l'hôte : $(hostname)"
 
-# Affichage du système d'exploitation
+
 echo "Système d'exploitation : $(uname -o)"
 
-# Affichage de la version du noyau
+
 echo "Version du noyau : $(uname -r)"
 
-# Affichage de l'architecture du système
+
 echo "Architecture : $(uname -m)"
 
-# Affichage de la mémoire disponible
+
 echo "Mémoire disponible :"
 free -h | grep "Mem:" | awk '{print $2 " de RAM disponible"}'
 
-# Affichage de l'espace disque disponible
+
 echo "Espace disque disponible :"
 df -h | grep "^/dev" | awk '{print $6 ": " $4 " disponibles"}'
--- lecture_script/lecture_script_project/src/BashScriptExecutor.java
+++ lecture_script/lecture_script_project/src/BashScriptExecutor.java
@@ -7,30 +7,30 @@ import java.io.IOException;
 public class BashScriptExecutor {
     public static void main(String[] args) {
         try {
-            // Chemin vers le script bash
+
             List<String> command = new ArrayList<>();
             command.add("./lib/infos_systeme.sh");
 
-            // Création du processus
+
             ProcessBuilder builder = new ProcessBuilder(command);
             Process process = builder.start();
 
-            // Lecture de la sortie standard et d'erreur
+
             BufferedReader stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
             BufferedReader stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
 
-            // Affichage de la sortie du script
+
             String line;
             while ((line = stdInput.readLine()) != null) {
                 System.out.println(line);
             }
 
-            // Affichage des erreurs du script, le cas échéant
+
             while ((line = stdError.readLine()) != null) {
                 System.err.println(line);
             }
 
-            // Attente de la fin du script
+
             process.waitFor();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();

55c29a4d46f1c69f2289dec5d5f0f60d3fe3e113
axelbernard20 <axel.bernard@etudiant.univ-rennes.fr>
2024-02-09 11:34:39 +0100

    modif lexecution bash

--- lecture_script/lecture_script_project/src/BashScriptExecutor.java
+++ lecture_script/lecture_script_project/src/BashScriptExecutor.java
@@ -1,18 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class BashScriptExecutor {
-    public static void main(String[] args) {
+
+    public String executeBash(String file) {
         try {
 
+            //String file = "./lib/infos_systeme.sh";
             List<String> command = new ArrayList<>();
-            command.add("./lib/infos_systeme.sh");
+            command.add(file);
 
 
             ProcessBuilder builder = new ProcessBuilder(command);
+            //builder.directory(new File("/lib"));
             Process process = builder.start();
 
 
@@ -21,19 +25,23 @@ public class BashScriptExecutor {
 
 
             String line;
+            String lines="";
             while ((line = stdInput.readLine()) != null) {
+                lines+=line;
                 System.out.println(line);
             }
-
+            
 
             while ((line = stdError.readLine()) != null) {
                 System.err.println(line);
+                lines+=line;
             }
 
-
             process.waitFor();
+            return lines;
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+            return "Error file not executed";
         }
     }
 }

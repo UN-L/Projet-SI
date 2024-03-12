0f4341f31d0e78fe3be37dbefe7a605a01cf314c
axelbernard20 <axel.bernard@etudiant.univ-rennes.fr>
2024-03-12 10:03:30 +0100

    uptade 3.lecture script

--- Sprint_1/3.lecture_script/lecture_script_project/src/BashScriptExecutor.java
+++ Sprint_1/3.lecture_script/lecture_script_project/src/BashScriptExecutor.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.InputStreamReader;
 import java.io.IOException;
 
 public class BashScriptExecutor {
 
-    public String executeBash(String file) {
+    public static void main(String[] args) {
+        executeBash("./lib/infos_systeme.sh");
+    }
+
+    public static String executeBash(String file) {
         try {
 
             //String file = "./lib/infos_systeme.sh";
